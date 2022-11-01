9aec307ae125041196ceb5da5b43d0e0c138019d
Grimlock8 <105064843+Grimlock8@users.noreply.github.com>
2022-11-01 15:15:46 -0500

    Add files via upload
    Initial Commit.

---
+++ Rec-Dev-Two/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rec_Dev_Two.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
